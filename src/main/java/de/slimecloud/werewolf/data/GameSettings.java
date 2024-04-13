d4f91dd12e9392ac7a042f517ffd3894f71235eb
cyklon73 <cyklon698@gmail.com>
2024-04-13 10:43:02 +0200

    changed roles to List

--- src/main/java/de/slimecloud/werewolf/data/GameSettings.java
+++ src/main/java/de/slimecloud/werewolf/data/GameSettings.java
@@ -4,10 +4,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
 public class GameSettings {
 	private final int werewolfAmount = 2;
-	private final Role[] roles = Arrays.stream(Role.values()).filter(Role::isGenerate).toArray(Role[]::new);
+	private final List<Role> roles = Arrays.stream(Role.values()).filter(Role::isGenerate).toList();
 }
