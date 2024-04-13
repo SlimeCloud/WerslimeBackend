4f3070881155aa1a83e5edc7fb4e7303f5a204c0
MineKing <mineking9534@gmail.com>
2024-04-13 09:48:56 +0200

    Protect SettingsEndpoint

--- src/main/java/de/slimecloud/werewolf/api/endpoints/game/SettingsEndpoint.java
+++ src/main/java/de/slimecloud/werewolf/api/endpoints/game/SettingsEndpoint.java
@@ -1,9 +1,9 @@
 package de.slimecloud.werewolf.api.endpoints.game;
 
+import de.slimecloud.werewolf.api.AuthorizationInfo;
 import de.slimecloud.werewolf.api.ErrorResponse;
 import de.slimecloud.werewolf.api.ErrorResponseType;
 import de.slimecloud.werewolf.api.Server;
-import de.slimecloud.werewolf.data.Game;
 import de.slimecloud.werewolf.data.GameSettings;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
@@ -11,7 +11,6 @@ import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 public class SettingsEndpoint implements Handler {
-
 	@Getter
 	public static class Request {
 		public GameSettings settings;
@@ -19,11 +18,11 @@ public class SettingsEndpoint implements Handler {
 
 	@Override
 	public void handle(@NotNull Context ctx) throws Exception {
-		Game game = ctx.appData(Server.MAIN_KEY).getGames().get(ctx.pathParam("id"));
+		AuthorizationInfo info = ctx.appData(Server.MAIN_KEY).getAuthenticator().checkAuthorization(ctx, true);
+		if (!info.getPlayer().isMaster()) throw new ErrorResponse(ErrorResponseType.MISSING_ACCESS);
 
-		if (game == null) throw new ErrorResponse(ErrorResponseType.GAME_NOT_FOUND);
+		if(!info.getGame().getId().toString().equals(ctx.pathParam("id"))) throw new ErrorResponse(ErrorResponseType.MISSING_ACCESS);
 
-		game.setSettings(ctx.bodyAsClass(Request.class).getSettings());
-
+		info.getGame().setSettings(ctx.bodyAsClass(Request.class).getSettings());
 	}
 }
