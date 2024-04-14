a1613474d23ce2e5265b9b1bceb65b3c510b411c
cyklon73 <cyklon698@gmail.com>
2024-04-14 13:52:46 +0200

    parse role specific requests

--- src/main/java/de/slimecloud/werewolf/data/Role.java
+++ src/main/java/de/slimecloud/werewolf/data/Role.java
@@ -1,5 +1,7 @@
 package de.slimecloud.werewolf.data;
 
+import de.slimecloud.werewolf.data.request.TargetRequest;
+import de.slimecloud.werewolf.data.request.WitchRequest;
 import io.javalin.http.Context;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,24 +15,28 @@ public enum Role {
 	WITCH(true, 3) {
 		@Override
 		public void handle(@NotNull Game game, @NotNull Player player, @NotNull Context ctx) {
-			super.handle(game, player, ctx);
+			WitchRequest request = ctx.bodyValidator(WitchRequest.class).get();
+			//TODO implement role logic
 		}
 	},
 	HUNTER(true, 1) {
 		@Override
 		public void handle(@NotNull Game game, @NotNull Player player, @NotNull Context ctx) {
-			super.handle(game, player, ctx);
+			TargetRequest request = ctx.bodyValidator(TargetRequest.class).get();
+			//TODO implement role logic
 		}
 	},
 	SEER(true, 2) {
 		@Override
 		public void handle(@NotNull Game game, @NotNull Player player, @NotNull Context ctx) {
-			super.handle(game, player, ctx);
+			TargetRequest request = ctx.bodyValidator(TargetRequest.class).get();
+			//TODO implement role logic
 		}
 	};
 
 	private final boolean special;
 	private final int priority;
+
 
 	public void handle(@NotNull Game game, @NotNull Player player, @NotNull Context ctx) {
 
