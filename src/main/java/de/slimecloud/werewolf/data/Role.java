9276c17cd5dd890f15f761da2f666c1736da7b66
cyklon73 <cyklon698@gmail.com>
2024-04-14 14:09:14 +0200

    implemented Id validation and alive check

--- src/main/java/de/slimecloud/werewolf/data/Role.java
+++ src/main/java/de/slimecloud/werewolf/data/Role.java
@@ -1,8 +1,11 @@
 package de.slimecloud.werewolf.data;
 
+import de.slimecloud.werewolf.api.ErrorResponse;
+import de.slimecloud.werewolf.api.ErrorResponseType;
 import de.slimecloud.werewolf.data.request.TargetRequest;
 import de.slimecloud.werewolf.data.request.WitchRequest;
 import io.javalin.http.Context;
+import kotlin.jvm.functions.Function1;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
@@ -40,5 +43,13 @@ public enum Role {
 
 	public void handle(@NotNull Game game, @NotNull Player player, @NotNull Context ctx) {
 
+	}
+
+	private static <T extends TargetRequest> Function1<T, Boolean> validateId(@NotNull Game game) {
+		return request -> game.getPlayers().containsKey(request.getId());
+	}
+
+	private static void checkAlive(Player player, boolean shouldAlive) {
+		if (player.isAlive() != shouldAlive) throw new ErrorResponse(ErrorResponseType.INVALID_TARGET);
 	}
 }
