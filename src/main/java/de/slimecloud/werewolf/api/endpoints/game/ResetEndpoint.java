676670fbba6659d090b56171cd76fb524874652c
MineKing <mineking9534@gmail.com>
2024-04-14 17:49:35 +0200

    Add reset endpoint

---
+++ src/main/java/de/slimecloud/werewolf/api/endpoints/game/ResetEndpoint.java
@@ -0,0 +1,19 @@
+package de.slimecloud.werewolf.api.endpoints.game;
+
+import de.slimecloud.werewolf.api.AuthorizationInfo;
+import de.slimecloud.werewolf.api.ErrorResponse;
+import de.slimecloud.werewolf.api.ErrorResponseType;
+import de.slimecloud.werewolf.api.Server;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+public class ResetEndpoint implements Handler {
+	@Override
+	public void handle(@NotNull Context ctx) throws Exception {
+		AuthorizationInfo info = ctx.appData(Server.MAIN_KEY).getAuthenticator().checkAuthorization(ctx, true);
+		if(!info.getPlayer().isMaster()) throw new ErrorResponse(ErrorResponseType.MISSING_ACCESS);
+
+		info.getGame().reset(true);
+	}
+}
