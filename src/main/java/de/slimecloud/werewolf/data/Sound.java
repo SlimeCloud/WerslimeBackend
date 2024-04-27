3b4fdc4d3b1400d90fc138415997725295079b8b
cyklon73 <cyklon698@gmail.com>
2024-04-27 21:08:39 +0200

    added ACTIVE_TURN and HOWL Sound

--- src/main/java/de/slimecloud/werewolf/data/Sound.java
+++ src/main/java/de/slimecloud/werewolf/data/Sound.java
@@ -3,14 +3,26 @@ package de.slimecloud.werewolf.data;
 import org.jetbrains.annotations.NotNull;
 
 public enum Sound {
+
 	/**
 	 * Played on Death
 	 */
 	DEATH,
+
 	/**
 	 * Played when amor makes two people fall in love with each other
 	 */
-	LOVE;
+	LOVE,
+
+	/**
+	 * Played when it is the current player's turn to play
+	 */
+	ACTIVE_TURN,
+
+	/**
+	 * Played when it's the WereSlime's turn
+	 */
+	HOWL;
 
 	public Data data(float volume) {
 		return new Data(this, volume);
