32e140beb82b8e7f66766ac2b56dd7f9a2677839
MineKing <mineking9534@gmail.com>
2024-04-27 21:22:28 +0200

    Add Winner#isMember

--- src/main/java/de/slimecloud/werewolf/data/Winner.java
+++ src/main/java/de/slimecloud/werewolf/data/Winner.java
@@ -7,14 +7,19 @@ import java.util.function.Predicate;
 
 @RequiredArgsConstructor
 public enum Winner {
-	LOVER(game -> game.getPlayers().values().stream().filter(Player::isAlive).filter(Player::isLover).count() >= game.getPlayers().values().stream().filter(Player::isAlive).count() && game.getPlayers().values().stream().filter(Player::isAlive).filter(p -> !p.isLover()).noneMatch(p -> p.getRole().isKilling())),
-	WEREWOLF(game -> game.getPlayers().values().stream().filter(Player::isAlive).allMatch(p -> p.getRole().getTeam() == Team.HOSTILE)),
-	VILLAGER(game -> game.getPlayers().values().stream().filter(Player::isAlive).noneMatch(p -> p.getRole().isKilling())),
-	JESTER(game -> false); //Player#kill
+	LOVER(Player::isLover, game -> game.getPlayers().values().stream().filter(Player::isAlive).filter(Player::isLover).count() >= game.getPlayers().values().stream().filter(Player::isAlive).count() && game.getPlayers().values().stream().filter(Player::isAlive).filter(p -> !p.isLover()).noneMatch(p -> p.getRole().isKilling())),
+	WEREWOLF(Role.WEREWOLF::hasRole, game -> game.getPlayers().values().stream().filter(Player::isAlive).allMatch(p -> p.getRole().getTeam() == Team.HOSTILE)),
+	VILLAGER(p -> !Role.WEREWOLF.hasRole(p), game -> game.getPlayers().values().stream().filter(Player::isAlive).noneMatch(p -> p.getRole().isKilling())),
+	JESTER(Role.WEREWOLF::hasRole, game -> false); //Player#kill
 
-	private final Predicate<Game> condition;
+	private final Predicate<Player> memberCondition;
+	private final Predicate<Game> winCondition;
+
+	public boolean isMember(@NotNull Player player) {
+		return memberCondition.test(player);
+	}
 
 	public boolean isWinning(@NotNull Game game) {
-		return condition.test(game);
+		return winCondition.test(game);
 	}
 }
