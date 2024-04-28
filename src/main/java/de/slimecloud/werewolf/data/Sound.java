3b8940eb3e9169b0e439e08b982e2dc42ff50e1b
MineKing <mineking9534@gmail.com>
2024-04-28 12:52:26 +0200

    Reformat code

--- src/main/java/de/slimecloud/werewolf/data/Sound.java
+++ src/main/java/de/slimecloud/werewolf/data/Sound.java
@@ -42,11 +42,12 @@ public enum Sound {
 	private final int variantAmount;
 
 	public Data data(double volume) {
-		return data(volume, Main.random.nextInt(variantAmount+1));
+		return data(volume, Main.random.nextInt(variantAmount + 1));
 	}
 
 	public Data data(double volume, int variant) {
 		if (volume < 0 || volume > 1) throw new IllegalArgumentException("Volume should be between 0 and 1. Volume " + volume + " is out of range");
+
 		if (variantAmount > 1 && variant == -1) variant = 0;
 		if (variantAmount == 1 && variant == 0) variant = -1;
 		return new Data(this, volume, variant);
