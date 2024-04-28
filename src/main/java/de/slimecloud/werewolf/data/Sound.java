f3132643e4ffcccb311283f8ff8812d6a59fd1ec
cyklon73 <cyklon698@gmail.com>
2024-04-28 12:51:03 +0200

    implemented Sound variants

--- src/main/java/de/slimecloud/werewolf/data/Sound.java
+++ src/main/java/de/slimecloud/werewolf/data/Sound.java
@@ -1,45 +1,58 @@
 package de.slimecloud.werewolf.data;
 
+import de.slimecloud.werewolf.main.Main;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
+@Getter
+@RequiredArgsConstructor
 public enum Sound {
 
 	/**
 	 * Played on Death
 	 */
-	DEATH,
+	DEATH(1),
 
 	/**
 	 * Played when amor makes two people fall in love with each other
 	 */
-	LOVE,
+	LOVE(1),
 
 	/**
 	 * Played when it is the current player's turn to play
 	 */
-	ACTIVE_TURN,
+	ACTIVE_TURN(1),
 
 	/**
 	 * Played when it's the WereSlime's turn
 	 */
-	HOWL,
+	HOWL(1),
 
 	/**
 	 * Played when Player wins the game
 	 */
-	WIN,
+	WIN(1),
 
 	/**
 	 * Played when it's the Villager's turn
 	 */
-	VILLAGER;
+	VILLAGER(2);
+
+	private final int variantAmount;
 
 	public Data data(double volume) {
-		if (volume < 0 || volume > 1) throw new IllegalArgumentException("Volume should be between 0 and 1. Volume " + volume + " is out of range");
-		return new Data(this, volume);
+		return data(volume, Main.random.nextInt(variantAmount+1));
 	}
 
-	public record Data(@NotNull Sound sound, double volume) {
+	public Data data(double volume, int variant) {
+		if (volume < 0 || volume > 1) throw new IllegalArgumentException("Volume should be between 0 and 1. Volume " + volume + " is out of range");
+		if (variantAmount > 1 && variant == -1) variant = 0;
+		if (variantAmount == 1 && variant == 0) variant = -1;
+		return new Data(this, volume, variant);
+	}
+
+	public record Data(@NotNull Sound sound, double volume, int variant) {
 
 	}
 }
