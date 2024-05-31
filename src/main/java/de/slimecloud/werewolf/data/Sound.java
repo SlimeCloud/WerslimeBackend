1c2fbf832c7bd32b62493fa8181719a6f2535c6c
MineKing <mineking9534@gmail.com>
2024-05-31 10:26:04 +0200

    Fix sound

--- src/main/java/de/slimecloud/werewolf/data/Sound.java
+++ src/main/java/de/slimecloud/werewolf/data/Sound.java
@@ -16,27 +16,29 @@ public enum Sound {
 	LOVE,
 
 	ACTIVE_TURN,
-	WARLOCK,
+	WARLOCK(2),
 	HOWL,
 	POTION,
 	SEER,
 	SHOOT,
-	VILLAGER;
+	VILLAGER(2);
 
-	private final int variantAmount;
+	private final int variants;
 
 	Sound() {
 		this(1);
 	}
 
 	public Data data(double volume) {
-		return data(volume, Main.random.nextInt(variantAmount));
+		return data(volume, Main.random.nextInt(variants));
 	}
 
 	public Data data(double volume, int variant) {
 		if (volume < 0 || volume > 1) throw new IllegalArgumentException("Volume should be between 0 and 1. Volume " + volume + " is out of range");
-		if (variantAmount > 1 && variant == -1) variant = 0;
-		if (variantAmount == 1 && variant == 0) variant = -1;
+
+		if (variants > 1 && variant == -1) variant = 0;
+		if (variants == 1 && variant == 0) variant = -1;
+
 		return new Data(this, volume, variant);
 	}
 
